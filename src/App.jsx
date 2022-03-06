70b5baefe713d7a958b5594893c57e9d4f35a43b
会飞的肥牛 <469235606@qq.com>
2022-03-06 13:26:50 +0800

    我的提交

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Routes, Route,Navigate } from 'react-router-dom'
 import routes from './config/routers'
+import Yltest from './pages/Yltest'
 
 
 export default function App() {
   return (
     <div> 
+      <Yltest name="yl" />
       <Routes>
         {routes.map((routeObj) => {
           return <Route key={routeObj.path} {...routeObj}/>
